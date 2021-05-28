4f2edbb3d5432395245b4f1892b613e3f4c41cad
Bryce B <bryce.boddy@gmail.com>
2021-05-28 12:25:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -14,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@items.length() <= 0
+      if @@cart.empty?
         resp.write "Your cart is empty"
       end
     else
